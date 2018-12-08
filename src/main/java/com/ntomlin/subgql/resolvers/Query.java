5262e11077f73f428bf1389269febda5d16a6e2e
Nick Tomlin <ntomlin@netflix.com>
2018-12-08 14:49:49 -0800

    Add a snippet to reproduce input type error

--- src/main/java/com/ntomlin/subgql/resolvers/Query.java
+++ src/main/java/com/ntomlin/subgql/resolvers/Query.java
@@ -3,6 +3,9 @@ package com.ntomlin.subgql.resolvers;
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 import org.springframework.stereotype.Component;
 
+import java.util.Collections;
+import java.util.List;
+
 /*
     our root Query resolver. This is automatically wired up to the schema define din resources/schema/*.graphqls
     by graphql-java-tools.
@@ -11,7 +14,22 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class Query implements GraphQLQueryResolver {
-    public String getTest () {
-        return "Test";
-    }
+    List<Material> materials(MaterialFilter filter) { return Collections.emptyList(); }
+}
+
+class Material {
+    Long id;
+}
+
+class MaterialFilter {
+    String title;
+    RequestFilter requestFilter;
+}
+class RequestFilter {
+    List<RequestFilter> and;
+    List<RequestFilter> or;
+    DiscountTypeFilter discountTypeFilter;
+}
+class DiscountTypeFilter {
+    String name;
 }
